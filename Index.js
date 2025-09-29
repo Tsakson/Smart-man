1bf29bcc5067ef7a818ef2ab9d01416c54636694
Tsakson <Tsaku2all@gmail.com>
2025-09-29 19:32:16 +0100

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,19 @@
+const display = document.getElementById('display');
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+function clearDisplay() {
+    display.value = '';
+}
+function calculateResult() {
+    try {eval
+        (display.value);
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
